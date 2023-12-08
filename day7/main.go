fc244dccc1de06e806c972b4f211d40ca46eb690
nodonoghue <nodonoghue@sightlinepayments.com>
2023-12-07 23:21:37 -0800

    Stuck on part 2

--- day7/main.go
+++ day7/main.go
@@ -61,6 +61,7 @@ func main() {
 	fmt.Println("Advent of code 2023: Day7")
 	hands := GetInputs("inputs.txt")
 	PartOne(hands)
+	PartTwo(hands)
 }
 
 func GetInputs(fileName string) []Hand {
@@ -112,7 +113,7 @@ func PartOne(hands []Hand) {
 	var calculatedHands []Hand
 
 	for _, currentHand := range hands {
-		calculatedHands = append(calculatedHands, CalculateHandScore(currentHand))
+		calculatedHands = append(calculatedHands, CalculateHandScore(currentHand, false))
 	}
 	sort.Slice(calculatedHands, func(i, j int) bool {
 		var returnObj bool
@@ -139,19 +140,52 @@ func PartOne(hands []Hand) {
 
 func PartTwo(hands []Hand) {
 	fmt.Println("Starting Part Two")
+	var answer int
+	var calculatedHands []Hand
 
+	for _, currentHand := range hands {
+		calculatedHands = append(calculatedHands, CalculateHandScore(currentHand, true))
+	}
+	sort.Slice(calculatedHands, func(i, j int) bool {
+		var returnObj bool
+		if calculatedHands[i].Rank == calculatedHands[j].Rank {
+			//walk the cards in each hand to see when one has a higher card
+			for index := range calculatedHands[i].Cards {
+				if CardScore()[calculatedHands[i].Cards[index]] == CardScore()[calculatedHands[j].Cards[index]] {
+					continue
+				} else {
+					returnObj = CardScore()[calculatedHands[i].Cards[index]] < CardScore()[calculatedHands[j].Cards[index]]
+					break
+				}
+			}
+		} else {
+			returnObj = calculatedHands[i].Rank < calculatedHands[j].Rank
+		}
+
+		return returnObj
+	})
+
+	answer = CalculateTotalScore(calculatedHands)
+	fmt.Println("Answer: ", answer)
 }
 
-func CalculateHandScore(hand Hand) Hand {
+func CalculateHandScore(hand Hand, hasWilds bool) Hand {
 	var returnObj Hand
-
-	rankObj := CalculateHandType(hand.Cards)
-	returnObj.Rank = rankObj.Rank
-	returnObj.CardScore = rankObj.RankScore
-	returnObj.Wager = hand.Wager
-	returnObj.Cards = hand.Cards
-
-	return returnObj
+	if hasWilds {
+		rankObj := CalculateHandTypeWild(hand.Cards)
+		returnObj.Rank = rankObj.Rank
+		returnObj.CardScore = rankObj.RankScore
+		returnObj.Wager = hand.Wager
+		returnObj.Cards = hand.Cards
+		return returnObj
+	} else {
+		rankObj := CalculateHandType(hand.Cards)
+		returnObj.Rank = rankObj.Rank
+		returnObj.CardScore = rankObj.RankScore
+		returnObj.Wager = hand.Wager
+		returnObj.Cards = hand.Cards
+		return returnObj
+	}
 }
 
 func CalculateHandType(cards []string) HandRank {
@@ -215,6 +249,89 @@ func CalculateHandTypeWild(cards []string) HandRank {
 	// find the highest scoring group and add all Js to it to make the best possible hand
 	// How to handle Full house?  if a Hand has 2 pairs and 1 J, add a J to the highest scoring
 	// group
+	var returnObj HandRank
+	countPair := 0
+	countTriple := 0
+	countQuad := 0
+	countQuint := 0
+	var groups []GroupScore
+
+	cardDict := make(map[string]int)
+	for _, card := range cards {
+		cardDict[card]++
+	}
+
+	wildCount := cardDict["J"]
+	delete(cardDict, "J")
+
+	for card, count := range cardDict {
+		switch count {
+		case 1:
+			groups = append(groups, GroupScore{card, count, CardScore()[card]})
+		case 2:
+			groups = append(groups, GroupScore{card, count, 2 * CardScore()[card]})
+		case 3:
+			groups = append(groups, GroupScore{card, count, 3 * CardScore()[card]})
+		case 4:
+			groups = append(groups, GroupScore{card, count, 4 * CardScore()[card]})
+		case 5:
+			groups = append(groups, GroupScore{card, count, 5 * CardScore()[card]})
+		}
+	}
+
+	sort.Slice(groups, func(i int, j int) bool {
+		return groups[i].Score > groups[j].Score
+	})
+
+	if (wildCount > 0 && wildCount < 5) && len(groups) > 0 {
+		groups[0].Count += wildCount
+		groups[0].Score = groups[0].Count * CardScore()[groups[0].Card]
+	} else if wildCount > 0 && len(groups) == 0 {
+		//groups = append(groups, GroupScore{cards[0], 2, 2 * CardScore()[cards[0]]})
+	} else if wildCount == 5 {
+		groups = append(groups, GroupScore{"A", 5, 5 * CardScore()["A"]})
+	}
+
+	for _, group := range groups {
+		switch group.Count {
+		case 2:
+			countPair++
+			returnObj.RankScore += group.Score
+		case 3:
+			countTriple++
+			returnObj.RankScore += group.Score
+		case 4:
+			countQuad++
+			returnObj.RankScore += group.Score
+		case 5:
+			countQuint++
+			returnObj.RankScore += group.Score
+		}
+	}
+
+	if countPair == 1 {
+		returnObj.Rank = HandScore()["OnePair"]
+	}
+	if countPair == 2 {
+		returnObj.Rank = HandScore()["TwoPair"]
+	}
+	if countTriple == 1 && countPair == 0 {
+		returnObj.Rank = HandScore()["ThreeOfAKind"]
+	}
+	if countTriple == 1 && countPair == 1 {
+		returnObj.Rank = HandScore()["FullHouse"]
+	}
+	if countQuad == 1 {
+		returnObj.Rank = HandScore()["FourOfAKind"]
+	}
+	if countQuint == 1 {
+		returnObj.Rank = HandScore()["FiveOfAKind"]
+	}
+	if countPair == 0 && countTriple == 0 && countQuad == 0 && countQuint == 0 {
+		returnObj.Rank = HandScore()["HighCard"]
+	}
+
+	return returnObj
 }
 
 func CalculateTotalScore(hands []Hand) int {
